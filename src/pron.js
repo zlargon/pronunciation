aaeacb0e34a2ed038ee2e7a1d3cbb68bb9fb7eb0
zlargon <zlargon@icloud.com>
2016-02-20 23:12:35 +0800

    fetch all the words and print KK, DJ pronunciations

---
+++ src/pron.js
@@ -0,0 +1,34 @@
+var coroutine = require('co');
+var commander = require('commander').Command;
+var pkg       = require('../package.json');
+var yahoo     = require('./yahoo');
+
+module.exports = coroutine.wrap(function * (process_argv) {
+  var program = new commander('pron');
+  program
+    .usage('<words...>')
+    .version(pkg.version)
+    .parse(process_argv);
+
+  // show help info if input is empty
+  if (process_argv.length <= 2) {
+    program.help();
+  }
+
+  for (var i = 0; i < program.args.length; i++) {
+    var word = program.args[i];
+
+    try {
+      var pron = yield yahoo(word);
+      console.log(word + ':');
+      console.log('KK ' + pron.kk);
+      console.log('DJ ' + pron.dj);
+      console.log('');
+
+    } catch (e) {
+      if (e.code === 'ENOENT') {
+        console.log(word + ' (Not Found)\n');
+      }
+    }
+  }
+});
